ffcb28ec229351665c6ecc99cf06db94ceb75949
Cristian B <cris.bahena77@gmail.com>
2024-01-11 19:34:46 -0600

    Add index action. New recipeindex file added to content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,23 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { RecipesIndex } from "./RecipesIndex"
+
 export function Content() {
+  const [recipes, setRecipes] = useState([]);
+
+  const handleIndexRecipes = () => {
+    console.log("handleIndexRecipes working");
+    axios.get("http://localhost:3000/recipes.json").then((response) => {
+    console.log(response.data);
+    setRecipes(response.data);
+  });
+  }
+
+  useEffect(handleIndexRecipes, []);
+
   return (
     <main>
-      <h1>Welcome to React!</h1>
+      <RecipesIndex recipes ={recipes}/>
     </main>
-  )
+  );
 }
---
+++ src/RecipesIndex.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+export function RecipesIndex (props) {
+
+  return(
+    <div>
+      <h1>All recipes</h1>
+      {props.recipes.map((recipe) => (
+         <div key={recipe.id}>
+           <h2>{recipe.name}</h2>
+           <h2>ID: {recipe.id}</h2>
+           <img src={recipe.image} />
+           <p>Decription: {recipe.decription}</p>
+           <p>Ingredients: {recipe.ingredients}</p>
+           <p>Instructions: {recipe.instructions}</p>
+         </div>
+       ))}
+    </div>
+  )
+}
